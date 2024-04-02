76fb0ffac5ad11a2382100b624f7314fc13c2e07
lanyangyangyeyeye dai <2871510993@qq.com>
2024-04-02 09:31:38 +0000

    try

--- lib/langguage.dart
+++ lib/langguage.dart
@@ -27,6 +27,54 @@ class Spacecraft{
       }
     }
 }
+//继承
+//继承spacecraft类
+class Orbiter extends Spacecraft {
+  double altitude;
+
+  Orbiter(super.name, DateTime super.launchDate, this.altitude);
+}
+//Mixins
+mixin Piloted{
+  int astronauts=1;
+  void describeCrew(){
+    print('Number of astronauts:$astronauts');
+  }
+}
+//利用Mixins方法继承这个类，将这个类的功能添加到其他类。
+class PilotedCraft extends Spacecraft with Piloted{
+  PilotedCraft(super.name, super.launchDate);
+  //.....
+}
+//星球类型枚举
+enum PlanetType { terrestrial, gas, ice }
+/// Enum that enumerates the different planets in our solar system
+/// and some of their properties.
+enum Planet {
+  earth(planetType: PlanetType.terrestrial, moons: 1, hasRings: false),
+
+  mercury(planetType: PlanetType.terrestrial, moons: 0, hasRings: false),
+  venus(planetType: PlanetType.terrestrial, moons: 0, hasRings: false),
+  // ···
+  uranus(planetType: PlanetType.ice, moons: 27, hasRings: true),
+  neptune(planetType: PlanetType.ice, moons: 14, hasRings: true);
+
+  /// A constant generating constructor
+  const Planet(
+      {required this.planetType, required this.moons, required this.hasRings});
+
+  /// All instance variables are final
+  final PlanetType planetType;
+  final int moons;
+  final bool hasRings;
+
+  /// Enhanced enums support getters and other methods
+  bool get isGiant =>
+      planetType == PlanetType.gas || planetType == PlanetType.ice;
+}
+
+final yourPlanet = Planet.earth;
+
 
 void main(){
 
@@ -47,7 +95,18 @@ void main(){
  voyager.describe();
  var voyager3=Spacecraft.unlaunched('Voyager III');
  voyager3.describe();
+//planet 枚举
+if (!yourPlanet.isGiant) {
+  print('Your planet is not a "giant planet".');
 }
+
+
+}
+
+
+//end main
+
+
 var year =1997;
 var antennaDiameter=3.7;
 var flybyObjects=['Jupiter','Saturn','Uranus','Neptune'];
